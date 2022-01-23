74fc2f374911bccdfaad7aa43be2db94197ca47e
Jaimie Jin <jinjaimie12@gmail.com>
2022-01-22 17:43:14 -0800

    db schema

--- server-code/db.js
+++ server-code/db.js
@@ -16,16 +16,46 @@ async function main() {
     submitter_email: String,
     paper_title: String,
     author: [String],
+    domain: [String],
+    subject: [String],
+    tech_system: Boolean,
+    goal: String,
+    goal_descr: String,
+    activity: String,
+    support: String,
+    interaction: String,
+    insight: String,
+    principles: String,
+    success: String,
+    unsuccess: String,
+    takaways: String,
+    // card summary
+    min_age: Number,
+    max_age: Number,
+    sub_domain: [String],
+    platform: String,
+    context: String,
+    research_title: String,
+    key_words: [String],
+    summary: String
 
   });
 
   db.FullCard = mongoose.model('FullCard', fullCardSchema);
 
-  const cardSchema = new mongoose.Schema({
-    full_card: {type: mongoose.Schema.Types.ObjectId, ref: 'FullCard'},
+//   const cardSchema = new mongoose.Schema({
+//     full_card: {type: mongoose.Schema.Types.ObjectId, ref: 'FullCard'},
+//     min_age: Number,
+//     max_age: Number,
+//     sub_domain: [String],
+//     platform: String,
+//     context: String,
+//     research_title: String,
+//     key_words: [String],
 
-  });
-  db.Card = mongoose.model('Card', cardSchema);
+
+//   });
+//   db.Card = mongoose.model('Card', cardSchema);
   
   console.log("finished mongodb schema");
 }
