545a6a7a4011dd94fc90c027d5c0deab4b269067
Jaimie Jin <jinjaimie12@gmail.com>
2022-01-22 17:42:55 -0800

    db schema

--- server-code/db.js
+++ server-code/db.js
@@ -9,14 +9,23 @@ async function main() {
   console.log(process.env.MONGODB_PWD)
   await mongoose.connect(`mongodb+srv://admin:${process.env.MONGODB_PWD}@capstone.66pej.mongodb.net/idctoolkit?retryWrites=true&w=majority`);
   console.log("connected to mongodb")
-  
+
+
+  const fullCardSchema = new mongoose.Schema({
+    submitter_name: String,
+    submitter_email: String,
+    paper_title: String,
+    author: [String],
+
+  });
+
+  db.FullCard = mongoose.model('FullCard', fullCardSchema);
+
   const cardSchema = new mongoose.Schema({
-    url: String,
-    description: String,
-    created_date: Date
+    full_card: {type: mongoose.Schema.Types.ObjectId, ref: 'FullCard'},
+
   });
   db.Card = mongoose.model('Card', cardSchema);
-
   
   console.log("finished mongodb schema");
 }
